16d04e02ba812ca19c96f020dd9bd0fe61c1a95a
VladGeekPro <juracovschi.vlad@gmail.com>
2026-04-21 16:00:09 +0300

    CreatedTmpHttpAudioReqeust

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
+use App\Http\Controllers\ExpenseVoiceTranscriptionDebugController;
 use App\Http\Controllers\ExpenseVoiceTranscriptionController;
 use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/admin/login');
+
+Route::get('/debug/expense-voice/transcribe', ExpenseVoiceTranscriptionDebugController::class)
+    ->name('debug.expense-voice.transcribe');
 
 Route::middleware('auth')->post('/expense-voice/transcriptions', ExpenseVoiceTranscriptionController::class)
     ->name('expense-voice.transcribe');
